af22ac1cc80b7ebdf68fb3f770da7f3984a58547
Maziyar Dowlatabadibazaz <maziyardowlat@gmail.com>
2021-09-07 23:07:09 -0700

    Changes

--- AngleCalc.py
+++ AngleCalc.py
@@ -1,23 +1,28 @@
-def boundTo180(angle):
-    """
-    :param angle: angle Input angle in degrees.
-    :return: The bounded angle in degrees.
-    """
-    while angle <= -180:
-        angle += 360
-    while angle > 180:
-        angle -= 360
-    return angle
+class AngleCalc:
+    def boundTo180(self, angle):
+        """
+        :param angle: angle Input angle in degrees.
+        :return: The bounded angle in degrees.
+        """
+        while angle <= -180:
+            angle += 360
+        while angle > 180:
+            angle -= 360
+        return angle
 
 
-def isAngleBetween(first_angle, middle_angle, second_angle):
-    if (first_angle > 180) or (middle_angle > 180) or second_angle > 180:
-        return False
-    elif first_angle <= middle_angle <= second_angle:
-        return True
-    elif second_angle <= middle_angle <= first_angle:
-        return True
-    else:
-        return False
-
-# Test
+    def isAngleBetween(self, first_angle, middle_angle, second_angle):
+        """
+        :param first_angle:
+        :param middle_angle:
+        :param second_angle:
+        :return:
+        """
+        if first_angle < 0:
+            first_angle += 360
+        if middle_angle < 0:
+            middle_angle += 360
+        if second_angle < 0:
+            second_angle += 360
+        return first_angle < middle_angle < second_angle or second_angle < middle_angle < first_angle and \
+               (abs(first_angle - middle_angle) < 90 or abs(second_angle - middle_angle) < 90)
--- Test.py
+++ Test.py
@@ -1,13 +1,26 @@
 import unittest
-import AngleCalc
-
+from AngleCalc import AngleCalc
 
 class TestCalc(unittest.TestCase):
 
     def test_angle(self):
-        result = AngleCalc.boundTo180(10)
+        angle_calc = AngleCalc()
+        result = angle_calc.boundTo180(10)
+        result2 = angle_calc.boundTo180(-280)
+        result3 = angle_calc.boundTo180(390)
+        result4 = angle_calc.boundTo180(0)
+        result5 = angle_calc.boundTo180(180)
+        result6 = angle_calc.boundTo180(-180)
         self.assertEqual(result, 10)
+        self.assertEqual(result2, 80)
+        self.assertEqual(result3, 30)
+        self.assertEqual(result4, 0)
+        self.assertEqual(result5, 180)
+        self.assertEqual(result6, 180)
 
     def test_cases(self):
-        result = AngleCalc.isAngleBetween(10, 1, 5)
-        self.assertTrue((10, 1, 5))
+        angle_calc = AngleCalc()
+        result3 = angle_calc.isAngleBetween(-90, -180, 110)
+        result4 = angle_calc.isAngleBetween(-90, -180, 80)
+        self.assertEqual(result3, True)
+        self.assertEqual(result4, False)
